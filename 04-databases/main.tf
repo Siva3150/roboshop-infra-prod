e8a6c284a7da53d1af598e041578f05ba102bdec
siva3150 <siva123@gmail.com>
2024-03-11 14:20:41 +0530

    jenkins

--- 04-databases/main.tf
+++ 04-databases/main.tf
@@ -40,7 +40,7 @@ resource "null_resource" "mongodb" {
     # Bootstrap script called with private_ip of each node in the cluster
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh mongodb dev"
+      "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
     ]
   }
 }
@@ -87,7 +87,7 @@ resource "null_resource" "redis" {
     # Bootstrap script called with private_ip of each node in the cluster
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh redis dev"
+      "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
     ]
   }
 }
@@ -135,7 +135,7 @@ resource "null_resource" "mysql" {
     # Bootstrap script called with private_ip of each node in the cluster
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh mysql dev"
+      "sudo sh /tmp/bootstrap.sh mysql ${var.environment}"
     ]
   }
 }
@@ -183,7 +183,7 @@ resource "null_resource" "rabbitmq" {
     # Bootstrap script called with private_ip of each node in the cluster
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh rabbitmq dev"
+      "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}"
     ]
   }
 }
@@ -195,7 +195,7 @@ module "records" {
 
   records = [
     {
-      name    = "mongodb-dev"
+      name    = "mongodb-${var.environment}"
       type    = "A"
       ttl     = 1
       records = [
@@ -203,7 +203,7 @@ module "records" {
       ]
     },
     {
-      name    = "redis-dev"
+      name    = "redis-${var.environment}"
       type    = "A"
       ttl     = 1
       records = [
@@ -211,7 +211,7 @@ module "records" {
       ]
     },
     {
-      name    = "mysql-dev"
+      name    = "mysql-${var.environment}"
       type    = "A"
       ttl     = 1
       records = [
@@ -219,7 +219,7 @@ module "records" {
       ]
     },
     {
-      name    = "rabbitmq-dev"
+      name    = "rabbitmq-${var.environment}"
       type    = "A"
       ttl     = 1
       records = [
